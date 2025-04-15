ec3a9ff9cf65acad0c203f1cdc05a54953cd0383
Era <60725430+dxking2005@users.noreply.github.com>
2025-04-15 01:28:36 +0000

    #

--- Day1.py
+++ Day1.py
@@ -1,10 +1,12 @@
-class person:
-    def __init__(self,name,age):
+class Person:
+    def __init__(self, name, age):
         self.name = name
         self.age = age
-    
+
     def myfunc(self):
-        print("Hello my name is" + self.name + "," + self.age)
-    
-    p1 = person("Bill",63)
-    p1.myfunc()
+        # 添加空格以美化输出格式
+        print(f"Hello my name is {self.name}, {self.age}")
+
+# 在类定义完成后进行实例化
+p1 = Person("Bill", 63)
+p1.myfunc()
